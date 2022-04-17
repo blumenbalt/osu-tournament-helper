bafa28607f218a40533f7d12004c0e85c1eb0953
arthur kenji <arthurhaguiuda@gmail.com>
2022-04-17 20:28:15 -0300

    cookie redone with client oauth token

--- app/Http/Controllers/CookieController.php
+++ app/Http/Controllers/CookieController.php
@@ -12,7 +12,7 @@ class CookieController extends Controller
      * 
      */
 
-    public static function getCookieAccessToken()
+    public static function getCookieUserAccessToken()
     {
         return Cookie::get('access_token');
     }
@@ -24,12 +24,38 @@ class CookieController extends Controller
      * 
      */
 
-    public static function setCookieAccessToken($result)
+    public static function setCookieUserAccessToken($result)
     {
         $minutes = $result->expires_in / 60;
         Cookie::queue('token_type', $result->token_type, $minutes);
         Cookie::queue('expires_in',  $result->expires_in, $minutes);
         Cookie::queue('access_token',  $result->access_token, $minutes);
         Cookie::queue('refresh_token', $result->refresh_token, $minutes);
+    }
+
+    /**
+     * 
+     * gets the access_token from the cookie to get data from client 
+     * 
+     */
+
+    public static function getCookieClientAccessToken()
+    {
+        return Cookie::get('access_token');
+    }
+
+    /**
+     * 
+     * 
+     * sets the token data on the cookie for retrieval of user data later
+     * 
+     */
+
+    public static function setCookieClientAccessToken($result)
+    {
+        $minutes = $result->expires_in / 60;
+        Cookie::queue('token_type', $result->token_type, $minutes);
+        Cookie::queue('expires_in',  $result->expires_in, $minutes);
+        Cookie::queue('access_token',  $result->access_token, $minutes);
     }
 }
