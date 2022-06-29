574f04b49fb1eeb78a97b3d13c5b06876529ac82
arthur kenji <arthurhaguiuda@gmail.com>
2022-06-29 20:23:59 -0300

    brackets model and migration done

---
+++ database/migrations/2022_06_29_224811_create_brackets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brackets', function (Blueprint $table) {
+            $table->id();
+            //@todo: check enums
+            $table->enum('round', ['', '']);
+            $table->foreign('tournament_id')->references('id')->on('tournaments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brackets');
+    }
+};
